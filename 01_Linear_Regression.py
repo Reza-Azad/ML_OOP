ab248a83311c782d81853f9fb29931df92f46a84
Reza Azadnajafabad <reza.azad.n@gmail.com>
2019-11-06 18:59:42 +0100

    minor changes

--- 01_Linear_Regression.py
+++ 01_Linear_Regression.py
@@ -28,3 +28,4 @@ model_input = model_input.reshape([1, 13])
 #create an instance of linear regression Predict class for loading the saved model
 Boston_Predict = ml.linear_regression_predict_class('linear_regression.sav', model_input)
 Boston_Predict.mpredict()
+
