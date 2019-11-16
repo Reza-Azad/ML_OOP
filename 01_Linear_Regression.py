8e6cffb9fb96880802469652bb3bf985fb1efb12
Reza Azadnajafabad <reza.azad.n@gmail.com>
2019-11-16 12:08:24 +0100

    codes from nongit path pasted to the git path

--- 01_Linear_Regression.py
+++ 01_Linear_Regression.py
@@ -3,5 +3,52 @@ In this file linear regression will be considered to do predictions
 also, an instance of Linear regression class will be used to be trained by the dataset.
 '''
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge
+
+#load csv file
+sales = pd.read_csv('kc_house_data.csv')
+#print shape and name of columns
+print('data frame shape: ', sales.shape)
+print('data frame columns: \n', sales.columns)
+#handeling the null values in the dataset
+print('print the number of null values in each column', sales.isnull().sum())
+# if there are some null values you have two options:
+#   1. fill the null fields with a space: modifiedSales = sales.fillna(“ “)
+#   2. omit the the row with null fields: modifiedSales = sales.dropna()
+
+#create a copy to protect the original dataset from manipulation
+sales_copy = sales.copy()
+#create feature dataset
+sales_features = sales_copy.drop(columns='price')
 
 
+
+
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(sales['sqft_living'], 
+                                                    sales['price'],
+                                                    test_size=.3,
+                                                    random_state = 0)
+
+
+print('type of X_train:', type(X_train))
+print('shape of X_train:', X_train.shape)
+print('shape of Y_train:', y_train.shape)
+X_train.reshape(15129, 1)
+y_train.reshape(15129, 1)
+print('shape of X_train:', X_train.shape)
+print('shape of Y_train:', y_train.shape)
+#create and fit linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+
+
+
+
+print('\n\nCode is Done\n')
